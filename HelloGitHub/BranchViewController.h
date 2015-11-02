d4e896b695feb28aa080f265e2fd50a976890c01
lisheng_xu <xls_man@foxmail.com>
2015-11-02 16:17:41 +0800

    branchTest
    Test

---
+++ HelloGitHub/BranchViewController.h
@@ -0,0 +1,13 @@
+//
+//  BranchViewController.h
+//  HelloGitHub
+//
+//  Created by 一卡易 on 15/11/2.
+//  Copyright © 2015年 1card1. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BranchViewController : UIViewController
+
+@end
---
+++ HelloGitHub/BranchViewController.m
@@ -0,0 +1,28 @@
+//
+//  BranchViewController.m
+//  HelloGitHub
+//
+//  Created by 一卡易 on 15/11/2.
+//  Copyright © 2015年 1card1. All rights reserved.
+//
+
+#import "BranchViewController.h"
+
+@interface BranchViewController ()
+
+@end
+
+@implementation BranchViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    NSLog(@"分支测试");
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
